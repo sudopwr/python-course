1091ce671b8562de49a077990b4bf0672e07f0d7
sudopwr <sudopwr@gmail.com>
2021-06-07 15:05:35 +0530

    add code for episode 112

---
+++ ep112.py
@@ -0,0 +1,15 @@
+yearlyString = input("Enter yearly installment: ")
+yearly = int(yearlyString)
+print(yearly / 12)
+
+"""
+=================
+"""
+
+kitkat = "10.35"
+parleg = "10"
+
+totalPay = kitkat + parleg
+print(totalPay)
+
+print(type(totalPay))
