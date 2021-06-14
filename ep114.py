7a69f2113a3d70e8ab8c22c079868b762772ae09
sudopwr <sudopwr@gmail.com>
2021-06-14 17:22:40 +0530

    add ep 115

---
+++ ep114.py
@@ -0,0 +1,6 @@
+expense = 100 # 100
+expense += 200 # 300
+print(expense) # 300
+
+expense = expense - 100 # 200
+print(expense) # 200
---
+++ ep115.py
@@ -0,0 +1,10 @@
+
+password = input("Enter password: ")
+rePassword = input("Re enter your password: ")
+
+print(password == rePassword)
+
+# student pass?
+mark = int(input("Enter marks: "))
+
+print("Pass: ", mark >= 35)
