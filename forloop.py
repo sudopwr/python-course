a4370f867a439832c2f8d90ab9e49300120d19cd
sudopwr <sudopwr@gmail.com>
2021-07-06 18:19:31 +0530

    for while loop

---
+++ forloop.py
@@ -0,0 +1,5 @@
+# multiplication table
+number = int(input("Enter number: "))
+
+for bar in range(1, 11, 1):
+    print(number, "x", bar, "=", number * bar)
---
+++ whileloop.py
@@ -0,0 +1,7 @@
+# multiplication table
+number = int(input("Enter number: "))
+
+bar = 1
+while bar <= 10:
+    print(number, "x", bar, "=", number * bar)
+    bar = bar + 1
